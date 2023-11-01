531b4bf91305bbf634017af4fce386b85a6d4fd9
Aleksandr Vorobev <47173672+B1boid@users.noreply.github.com>
2023-11-01 23:43:07 +0000

    upd

--- src/config/run_config.ts
+++ src/config/run_config.ts
@@ -41,7 +41,7 @@ export const MAINS_ZKSYNC_CONFIG: RunConfig = {
 export const ZKSYNC_ANOTHER_CONFIG: RunConfig = {
     strategy: Strategy.ZkSyncAnother,
     threads: 2,
-    waitInitial: {min: 0, max: 20 * 60},
-    waitBetweenTxs: {min: 30, max: 10 * 60},
+    waitInitial: {min: 0, max: 10 * 60},
+    waitBetweenTxs: {min: 30, max: 15 * 60},
     waitBetweenModules: {min: 0, max: 5 * 60}
 }
