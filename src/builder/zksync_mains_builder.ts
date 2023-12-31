ce6b166cf63464fa7cfea2a943f5fc80094ea9ad
Aleksandr Vorobev <47173672+B1boid@users.noreply.github.com>
2023-12-31 14:33:30 +0000

    add mains

--- src/builder/zksync_mains_builder.ts
+++ src/builder/zksync_mains_builder.ts
@@ -1,5 +1,5 @@
-import {ModuleActions, Randomness, WalletActions} from "../classes/actions";
-import {ZkSyncActivity} from "../module_blockchains/blockchain_modules";
+import {AnyActions, ModuleActions, Randomness, WalletActions} from "../classes/actions";
+import {ScrollActivity, ZkSyncActivity} from "../module_blockchains/blockchain_modules";
 import {getRandomElement, getRandomInt} from "../utils/utils";
 import {Blockchains} from "../config/chains";
 
@@ -7,9 +7,45 @@ export async function buildZkSyncMains(activeAddresses: string[]): Promise<Walle
     let res: WalletActions[] = []
     for (let addr of activeAddresses) {
         res.push({
-            actions: [generateZkSync()],
+            actions: getRandomInt(0, 100) < 50 ? [generateZkSync(), generateScroll()] : [generateScroll(), generateZkSync()],
             address: addr
         })
+    }
+    return res
+}
+
+function generateScroll(): ModuleActions {
+    let activities: ScrollActivity[] = generateScrollActivities(getRandomInt(2, 3))
+
+    return {
+        chainName: Blockchains.Scroll,
+        randomOrder: Randomness.Full,
+        activityNames: activities
+    }
+}
+
+function generateScrollActivities(activitiesNum: number): ScrollActivity[] {
+    let availableActivities: ScrollActivity[] = [
+        ScrollActivity.scrollDmail,
+        ScrollActivity.scrollWrapUnwrap,
+        ScrollActivity.scrollRandomStuff,
+        ScrollActivity.scrollRandomApprove,
+        ScrollActivity.scrollEmptyRouter,
+        ScrollActivity.scrollSwapCycleNativeToUsdc
+    ]
+
+    let res: ScrollActivity[] = []
+    let repeatedActivities: ScrollActivity[] = []
+    for (let i = 0; i < activitiesNum; i++) {
+        let curActivity: ScrollActivity
+        while (true) {
+            curActivity = getRandomElement(availableActivities)
+            if (!repeatedActivities.includes(curActivity)) {
+                break
+            }
+        }
+        res.push(curActivity)
+        repeatedActivities.push(curActivity)
     }
     return res
 }
@@ -18,20 +54,20 @@ export async function buildZkSyncMains(activeAddresses: string[]): Promise<Walle
 function generateZkSync(): ModuleActions {
     let activities: ZkSyncActivity[]
 
-    activities = generateZkSyncRandomActivities(getRandomInt(2, 3))
+    activities = generateZkSyncRandomActivities(getRandomInt(1, 2))
     // if (getRandomInt(0, 100) < 85) { // 85% chance to mint tevaera
     //     activities.push(ZkSyncActivity.zkSyncMintTevaera)
     // }
     // if (getRandomInt(0, 100) < 65) { // 65% chance to mint znsid
     //     activities.push(ZkSyncActivity.zkSyncMintZnsId)
     // }
-    if (getRandomInt(0, 100) < 25) { // 35% chance to enterMarketsZkEra
+    if (getRandomInt(0, 100) < 25) { // 25% chance to enterMarketsZkEra
         activities.push(ZkSyncActivity.zkSyncEraLendInit)
     }
-    if (getRandomInt(0, 100) < 25) { // 35% chance to enterMarketsReactFusion
+    if (getRandomInt(0, 100) < 25) { // 25% chance to enterMarketsReactFusion
         activities.push(ZkSyncActivity.zkSyncReactFusionInit)
     }
-    if (getRandomInt(0, 100) < 25) { // 35% chance to mint test tokens
+    if (getRandomInt(0, 100) < 25) { // 25% chance to mint test tokens
         activities.push(ZkSyncActivity.zkSyncSynFuturesTest)
     }
 
